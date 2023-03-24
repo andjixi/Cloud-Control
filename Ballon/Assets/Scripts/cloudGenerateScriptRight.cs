f1fa464cf11a24b3c4d7a464b089f0b72b19105d
comdora <bmjgavrilovic@gmail.com>
2023-03-24 23:25:22 +0100

    daj Boze da ovo uspe

---
+++ Ballon/Assets/Scripts/cloudGenerateScriptRight.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudGenerateScriptRight : MonoBehaviour
+{
+    public GameObject cloud;
+
+    public float spawnRate = 2;
+    private float timer = 0;
+
+    public float weightOffset = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnCloud();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer < spawnRate)
+            timer += Time.deltaTime;
+        else
+        {
+            spawnCloud();
+            timer = 0;
+        }
+    }
+
+    void spawnCloud()
+    {
+        float left = transform.position.x - weightOffset;
+        float right = transform.position.x + weightOffset;
+
+        Instantiate(cloud, new Vector3(Random.Range(left, right), transform.position.y, 0), transform.rotation);
+    }
+}
--- Ballon/Assets/Scripts/cloudMoveScript.cs
+++ Ballon/Assets/Scripts/cloudMoveScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class cloudMoveScript : MonoBehaviour
 {
-    public float moveSpeed = 3f;
+    public float moveSpeed = 8f;
+    public float deadZone = -15;
     
     // Start is called before the first frame update
     void Start()
@@ -17,5 +18,8 @@ public class cloudMoveScript : MonoBehaviour
     {
         Vector3 moveDir = new Vector3(0f, 1f, 0f);
         transform.position -= moveDir * moveSpeed * Time.deltaTime;
+
+        if(transform.position.y < deadZone)
+            Destroy(gameObject);
     }
 }
