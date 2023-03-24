a2b0af3cb06040792cb52c58c73e3ba629f8f642
MarkoPaunovic14 <116953405+MarkoPaunovic14@users.noreply.github.com>
2023-03-24 20:28:55 +0100

    Main

--- Ballon/Assets/Scripts/PlayerController.cs
+++ Ballon/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private float playerSpeed = 70f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
+       
+
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if(Input.GetKey(KeyCode.A)){
+            inputVector.x -= 1;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            inputVector.x += 1;
+        }
         
+        transform.position += (Vector3)inputVector * Time.deltaTime * playerSpeed;
+
+
     }
 }
